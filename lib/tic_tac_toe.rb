bc8cb838ff201b1a44b12f618567eefe74406d53
vy vo <tuonggvy.vo@gmail.com>
2019-10-19 23:56:43 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,44 @@
+#WIN_COMBINATIONS
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6],
+  ]
+  
+  #display_board
+  def display_board(board)
+    puts " #{board[0]} | #{board[1]} | #{board[2]} "
+    puts "-----------"
+    puts " #{board[3]} | #{board[4]} | #{board[5]} "
+    puts "-----------"
+    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  end
+  
+  #input_to_index
+  def input_to_index(user_input)
+    user_input.to_i - 1
+  end
+  
+  #move
+  def move(board,user_index,current_player)
+    board[user_index] = current_player
+  end
+  
+  #position_taken?
+  def position_taken?(board,user_index)
+    board[user_index] == "X" || board[user_index] == "O"
+  end
+  
+  #valid_move?
+  def valid_move?(board,user_index)
+    user_index.between?(0,8) && !position_taken?(board,user_index)
+  end
+  
+  #turn
+    
+    
