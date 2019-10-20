0b23fba219eb60df508bfc0d694630d23855fc20
vy vo <tuonggvy.vo@gmail.com>
2019-10-20 02:48:51 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -91,7 +91,7 @@ WIN_COMBINATIONS = [
       elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
         return win_combination
       else
-        return false
+        false
       end
     end
   end
