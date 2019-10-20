04c77500a32f9c8f35ee1da76bf738b4067a6894
vy vo <tuonggvy.vo@gmail.com>
2019-10-20 02:48:07 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -86,4 +86,13 @@ WIN_COMBINATIONS = [
       position_2 = board[index_1]
       position_3 = board[index_2]
       
-      if position_1 == "X" && position_2 == "X" && position_3 == "X" 
+      if position_1 == "X" && position_2 == "X" && position_3 == "X"
+        return win_combination
+      elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+        return win_combination
+      else
+        return false
+      end
+    end
+  end
+  
