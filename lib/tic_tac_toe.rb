e7cc11b869964a621a3012a69239a5c49d9a7cfa
vy vo <tuonggvy.vo@gmail.com>
2019-10-20 02:05:20 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -45,8 +45,9 @@ WIN_COMBINATIONS = [
       puts "Please choose a number between 1-9:"
       user_input = gets.strip
       input_to_index(user_input)
-      if valid_move?(board,user_index)
+      if user_input.between?(0,8) && valid_move?(board,user_index) == true
         move(board,user_index,current_player)
+        return 
       else
         turn(board)
       end
